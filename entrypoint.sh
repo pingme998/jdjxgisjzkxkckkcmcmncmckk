39d52cdd71e84f754fba4296a0432ca0d73d0bb4
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-10-15 16:52:43 +0530

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#author https://github.com/developeranaz
+rc version
+#curl $CONFIG_IN_URL >/root/.config/rclone/rclone.conf
+wget "$CONFIG_IN_URL" -O /.rclone.conf
+wget "$CONFIG_IN_URL" -O /root/.config/rclone/rclone.conf
+rc listremotes
+echo "$template" >.template
+cat .template |sed 's/dark/0/g' |sed 's/light/1/g' >.darklight
+if [ $(cat .darklight) -eq 0 ]
+then
+    echo "Theme:Dark"
+    thetheme="/usr/bin/.con/1.html"
+else
+    echo "Theme:Light"
+    thetheme="/usr/bin/.con/0.html"
+fi
+#rc auth
+echo "$indexauthentication" >.yesno
+cat .yesno |sed 's/false/0/g' |sed 's/true/1/g' >.auth
+if [ $(cat .auth) -eq 0 ]
+then
+    echo "no password, hence unautheticated index"
+    theindexpassword=''
+    theindexusername=''
+else
+    echo "oh! password, hence authenticated index"
+    theindexpassword="$INDEXpassword"
+    theindexusername="$INDEXusername"
+fi
+#next
+echo "$inde" >.yesno
+cat .yesno |sed 's/false/0/g' |sed 's/true/1/g' >.auth
+if [ $(cat .auth) -eq 0 ]
+then
+    echo "no password, hence unautheticated index"
+    theindexpassword=''
+    theindexusername=''
+else
+    echo "oh! password, hence authenticated index"
+    theindexpassword="$INDEXpassword"
+    theindexusername="$INDEXusername"
+fi
+
+
+rc rcd --rc-serve --rc-addr=0.0.0.0:$PORT --rc-pass="$theindexpassword" --rc-user="$theindexusername" --rc-template="$thetheme"
