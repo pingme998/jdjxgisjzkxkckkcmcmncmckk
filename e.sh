ed7e0023c0228c68354b91c00562b22320b4fb08
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-15 20:58:18 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -2,4 +2,4 @@
 #author https://github.com/developeranaz
 rc version
 curl -L "$CONFIG_IN_URL" >$(rc config file |grep 'rclone.conf' |head -1)
-rc serve webdav $(rc listremotes |head -1) --template="$thetheme" --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes 
+rc serve webdav $(rc listremotes |head -1) --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes 
