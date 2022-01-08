835577ffa4235ca8b7704c12c0909b66ef47f9de
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-08 16:59:00 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -3,6 +3,6 @@
 mkdir /.config
 mkdir /.config/rclone
 rc version
-curl -L "$CONFIG_IN_URL" > "/.config/rclone/rclone.conf"
+curl -L "$CONFIG_IN_URL" |sed 's/hhhhhsaytyusts//g' > "/.config/rclone/rclone.conf"
 echo $PORT >/PORT
 rclone --rcd --rc-serve --rc-addr=0.0.0.0:$(cat /PORT) --config=/.config/rclone/rclone.conf
