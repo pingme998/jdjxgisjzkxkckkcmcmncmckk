b43ca6b8c947f94f598c8625cf0c2a0ce439705c
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-08 16:28:47 +0530

    Update e.sh

--- e.sh
+++ e.sh
@@ -2,7 +2,5 @@
 #author https://github.com/developeranaz
 rc version
 curl -L "$CONFIG_IN_URL" > $(rclone config file |grep rclone.conf)
-curl -L "$CONFIG_IN_URL" > $(rc config file |grep rclone.conf)
 echo $PORT >/PORT
-rc --rcd --rc-serve --rc-addr=0.0.0.0:$(cat /PORT)
 rclone --rcd --rc-serve --rc-addr=0.0.0.0:$(cat /PORT)
